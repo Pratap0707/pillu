a3319d145c587b7b44baaa74af549b9295bf5b29
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-03-19 02:16:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ global.antitags = true
 //global.autoTyping = false //auto tying in gc (true to on, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
-global.Owner = ['918010431346','917798203711']
+global.Owner = ['918010431346','919394988360']
  //Number of owners //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.OwnerNumber = ['919394988360','919394988360']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.ownertag = ['919394988360','919394988360']
+global.OwnerNumber = ['918010431346','919394988360']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.ownertag = ['918010431346','919394988360']
 global.BotName = "𓄂⍣⃝D  e v i l ×͜×"
 global.packname = "A17 bot"
 global.author = "By: shubham"
